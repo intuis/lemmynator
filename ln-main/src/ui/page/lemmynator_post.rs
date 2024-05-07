5f7d65ff5e5433668688cec6242bb37fdd278344
Remigiusz Micielski <rmicielski@purelymail.com>
2024-05-07 16:07:03 +0200

    improve code

--- ln-main/src/ui/page/lemmynator_post.rs
+++ ln-main/src/ui/page/lemmynator_post.rs
@@ -17,19 +17,22 @@ pub struct LemmynatorPost {
     name: String,
     body: String,
     pub is_focused: bool,
-    image: Arc<Mutex<Option<Image>>>,
-    embed_description: Option<String>,
+    image_data: Arc<Mutex<Option<ImageData>>>,
     embed_url: Option<url::Url>,
     author: String,
     community: String,
-    downvotes: i64,
-    comments: i64,
-    upvotes: i64,
+    counts: LemmynatorCounts,
     is_featured_local: bool,
     is_featured_community: bool,
 }
 
-struct Image {
+struct LemmynatorCounts {
+    upvotes: i64,
+    downvotes: i64,
+    comments: i64,
+}
+
+struct ImageData {
     image: Box<dyn StatefulProtocol>,
     dimensions: (u32, u32),
 }
@@ -60,9 +63,7 @@ impl LemmynatorPost {
                     x
                 })
                 .collect()
-            // Paragraph::new(body).wrap(Wrap { trim: false })
-            // f.render_widget(text, text_rect);
-        } else if let Some(body) = lemmy_post.post.embed_description.clone() {
+        } else if let Some(body) = lemmy_post.post.embed_description {
             body.lines()
                 .filter(|line| !line.is_empty())
                 .map(|x| {
@@ -71,31 +72,213 @@ impl LemmynatorPost {
                     x
                 })
                 .collect()
-            // Paragraph::new(formatted_body.clone()).wrap(Wrap { trim: false })
         } else {
             "".to_string()
         };
-        // Line::default().spans(spans);
+
+        let counts = LemmynatorCounts {
+            upvotes: lemmy_post.counts.upvotes,
+            downvotes: lemmy_post.counts.downvotes,
+            comments: lemmy_post.counts.downvotes,
+        };
 
         LemmynatorPost {
             name: lemmy_post.post.name,
             body,
             community: lemmy_post.community.name,
             author: lemmy_post.creator.name,
-            embed_description: lemmy_post.post.embed_description,
             embed_url,
             is_focused: false,
-            image,
-            upvotes: lemmy_post.counts.upvotes,
-            downvotes: lemmy_post.counts.downvotes,
-            comments: lemmy_post.counts.comments,
+            image_data: image,
+            counts,
             is_featured_local: lemmy_post.post.featured_local,
             is_featured_community: lemmy_post.post.featured_community,
         }
     }
 
     fn is_image_only(&self) -> bool {
-        self.body.is_empty() && self.image.lock().unwrap().is_some()
+        self.body.is_empty() && self.image_data.lock().unwrap().is_some()
+    }
+
+    async fn fetch_image(url: String, image: Arc<Mutex<Option<ImageData>>>, ctx: Arc<Ctx>) {
+        let new_image = {
+            let res = ctx.client.get(url).send().await.unwrap();
+            Some(res.bytes().await.unwrap())
+        };
+
+        let new_image = if let Some(image) = new_image {
+            let dyn_image_res = image::io::Reader::new(Cursor::new(image))
+                .with_guessed_format()
+                .unwrap()
+                .decode();
+            if let Ok(dyn_image) = dyn_image_res {
+                dyn_image.dimensions();
+                Some(ImageData {
+                    dimensions: dyn_image.dimensions(),
+                    image: ctx.picker.lock().unwrap().new_resize_protocol(dyn_image),
+                })
+            } else {
+                None
+            }
+        } else {
+            None
+        };
+
+        if let Some(new_image) = new_image {
+            *image.lock().unwrap() = Some(new_image);
+        }
+        ctx.action_tx.send(Action::Render).unwrap();
+    }
+}
+
+impl Component for LemmynatorPost {
+    fn handle_actions(&mut self, _action: Action) -> Option<Action> {
+        None
+    }
+
+    fn render(&mut self, f: &mut Frame, rect: Rect) {
+        let inner_rect = rect.inner(&Margin::new(1, 1));
+
+        let post_block = self.post_block();
+        f.render_widget(post_block, rect);
+
+        if !self.is_image_only() {
+            let [_, image_rect, _, mut text_rect] = Layout::horizontal([
+                Constraint::Length(1),
+                Constraint::Length(20),
+                Constraint::Length(1),
+                Constraint::Percentage(75),
+            ])
+            .areas(inner_rect);
+
+            if let Some(image) = &mut *self.image_data.lock().unwrap() {
+                let image_widget = StatefulImage::new(None);
+                f.render_stateful_widget(image_widget, image_rect, &mut image.image);
+            } else {
+                text_rect = inner_rect;
+            }
+
+            let mut there_was_a_header = false;
+            let lines: Vec<_> = self
+                .body
+                .lines()
+                .map(|line| {
+                    if line.starts_with("#") {
+                        let trimmed_line = line.trim_start_matches('#');
+                        there_was_a_header = true;
+                        vec![Line::styled(trimmed_line, Style::new().bold())]
+                    } else {
+                        if there_was_a_header {
+                            let rect_len = text_rect.width - 2;
+                            let mut result = String::with_capacity(
+                                line.len() + line.len() / rect_len as usize + 2,
+                            );
+                            let mut count = 0;
+
+                            let mut lines = vec![];
+                            for char in line.chars() {
+                                if count < rect_len {
+                                    if count == 0 {
+                                        result.push(' ');
+                                        result.push(' ');
+                                    }
+                                    result.push(char);
+                                    count += 1;
+                                } else {
+                                    result.push('\n');
+                                    lines.push(Line::from(result.clone()));
+                                    result.clear();
+                                    count = 0;
+                                }
+                            }
+                            lines
+                        } else {
+                            vec![Line::raw(line)]
+                        }
+                    }
+                })
+                .collect();
+
+            let mut new_lines = vec![];
+            for line in lines {
+                for line in line {
+                    new_lines.push(line);
+                }
+            }
+
+            let body_paragraph = Paragraph::new(new_lines).wrap(Wrap { trim: false });
+            f.render_widget(body_paragraph, text_rect);
+        } else {
+            let left_padding_percentage = {
+                let (width, height) = self.image_data.lock().unwrap().as_ref().unwrap().dimensions;
+                if width > height {
+                    40
+                } else {
+                    45
+                }
+            };
+            let [_, image_rect, _] = Layout::horizontal([
+                Constraint::Percentage(left_padding_percentage),
+                Constraint::Percentage(45),
+                Constraint::Percentage(10),
+            ])
+            .areas(inner_rect);
+            if let Some(image) = &mut *self.image_data.lock().unwrap() {
+                let image_state = StatefulImage::new(None);
+                f.render_stateful_widget(image_state, image_rect, &mut image.image);
+            }
+        }
+
+        self.is_focused = false;
+    }
+}
+
+// UI render related things
+impl LemmynatorPost {
+    fn post_block(&self) -> Block {
+        Block::bordered()
+            .border_type(BorderType::Rounded)
+            .border_style(self.border_style())
+            .title(Title::from(self.header()).alignment(Alignment::Left))
+            .title(
+                Title::from(self.footer_right())
+                    .alignment(Alignment::Right)
+                    .position(Position::Bottom),
+            )
+    }
+
+    fn border_style(&self) -> Style {
+        if self.is_focused {
+            Style::default().fg(Color::Magenta)
+        } else {
+            Style::default()
+        }
+    }
+
+    fn footer_right(&self) -> Line {
+        let spans = vec![
+            Span::styled(
+                format!(" c/{}  u/{}  ", self.community, self.author),
+                Style::new().white(),
+            ),
+            Span::styled(format!(" {} ", self.counts.upvotes), Style::new().green()),
+            Span::styled(format!(" "), Style::new().white()),
+            Span::styled(format!(" {}", self.counts.downvotes), Style::new().red()),
+            Span::styled(format!(" "), Style::new().white()),
+            Span::styled(format!("󰆉 {} ", self.counts.comments), Style::new().white()),
+        ];
+
+        let line = if self.is_focused {
+            let spans: Vec<_> = spans
+                .into_iter()
+                .map(|span| span.patch_style(Style::new().bold()))
+                .collect();
+            Line::default().spans(spans)
+        } else {
+            Line::default().spans(spans)
+        };
+
+        line
     }
 
     fn header(&self) -> Line {
@@ -134,192 +317,5 @@ impl LemmynatorPost {
         };
 
         Line::default().spans(spans)
-    }
-
-    fn footer_right(&self) -> Line {
-        // Line::default().spans(vec![format!(
-        //     " c/{}  u/{}  ",
-        //     self.community, self.author
-        // )]);
-        let spans = vec![
-            Span::styled(
-                format!(" c/{}  u/{}  ", self.community, self.author),
-                Style::new().white(),
-            ),
-            Span::styled(format!(" {} ", self.upvotes), Style::new().green()),
-            Span::styled(format!(" "), Style::new().white()),
-            Span::styled(format!(" {}", self.downvotes), Style::new().red()),
-            Span::styled(format!(" "), Style::new().white()),
-            Span::styled(format!("󰆉 {} ", self.comments), Style::new().white()),
-        ];
-
-        let line = if self.is_focused {
-            let spans: Vec<_> = spans
-                .into_iter()
-                .map(|span| span.patch_style(Style::new().bold()))
-                .collect();
-            Line::default().spans(spans)
-        } else {
-            Line::default().spans(spans)
-        };
-
-        line
-        // format!(
-        //     " c/{}  u/{}   {}  {}  󰆉 {} ",
-        //     self.community,
-        //     self.author,
-        //     self.upvotes.to_string().green(),
-        //     self.downvotes.to_string().red(),
-        //     self.comments
-        // )
-    }
-
-    async fn fetch_image(url: String, image: Arc<Mutex<Option<Image>>>, ctx: Arc<Ctx>) {
-        let new_image = {
-            let res = ctx.client.get(url).send().await.unwrap();
-            Some(res.bytes().await.unwrap())
-        };
-
-        let new_image = if let Some(image) = new_image {
-            let dyn_image_res = image::io::Reader::new(Cursor::new(image))
-                .with_guessed_format()
-                .unwrap()
-                .decode();
-            if let Ok(dyn_image) = dyn_image_res {
-                dyn_image.dimensions();
-                Some(Image {
-                    dimensions: dyn_image.dimensions(),
-                    image: ctx.picker.lock().unwrap().new_resize_protocol(dyn_image),
-                })
-            } else {
-                None
-            }
-        } else {
-            None
-        };
-
-        if let Some(new_image) = new_image {
-            *image.lock().unwrap() = Some(new_image);
-        }
-        ctx.action_tx.send(Action::Render).unwrap();
-    }
-}
-
-impl Component for LemmynatorPost {
-    fn handle_actions(&mut self, _action: Action) -> Option<Action> {
-        None
-    }
-
-    fn render(&mut self, f: &mut Frame, rect: Rect) {
-        let inner_rect = rect.inner(&Margin::new(1, 1));
-
-        let border_style = {
-            if self.is_focused {
-                Style::default().fg(Color::Magenta)
-            } else {
-                Style::default()
-            }
-        };
-
-        let block = Block::bordered()
-            .border_type(BorderType::Rounded)
-            .border_style(border_style)
-            .title(Title::from(self.header()).alignment(Alignment::Left))
-            .title(
-                Title::from(self.footer_right())
-                    .alignment(Alignment::Right)
-                    .position(Position::Bottom),
-            );
-
-        f.render_widget(block, rect);
-
-        if !self.is_image_only() {
-            let [_, image_rect, _, mut text_rect] = Layout::horizontal([
-                Constraint::Length(1),
-                Constraint::Length(20),
-                Constraint::Length(1),
-                Constraint::Percentage(75),
-            ])
-            .areas(inner_rect);
-
-            if let Some(image) = &mut *self.image.lock().unwrap() {
-                let image_state = StatefulImage::new(None);
-                f.render_stateful_widget(image_state, image_rect, &mut image.image);
-            } else {
-                text_rect = inner_rect;
-            }
-
-            let mut there_was_a_header = false;
-            let lines: Vec<_> = self
-                .body
-                .lines()
-                .map(|line| {
-                    if line.starts_with("#") {
-                        let trimmed_line = line.trim_start_matches('#');
-                        there_was_a_header = true;
-                        vec![Line::styled(trimmed_line, Style::new().bold())]
-                    } else {
-                        if there_was_a_header {
-                            let rect_len = text_rect.width - 2;
-                            let mut result = String::with_capacity(
-                                line.len() + line.len() / rect_len as usize + 2,
-                            );
-                            let mut count = 0;
-
-                            let mut lines = vec![];
-                            for (i, char) in line.char_indices() {
-                                if count < rect_len {
-                                    if count == 0 {
-                                        result.push(' ');
-                                        result.push(' ');
-                                    }
-                                    result.push(char);
-                                    count += 1;
-                                } else {
-                                    result.push('\n');
-                                    lines.push(Line::from(result.clone()));
-                                    result.clear();
-                                    count = 0;
-                                }
-                            }
-                            lines
-                        } else {
-                            vec![Line::raw(line)]
-                        }
-                    }
-                })
-                .collect();
-
-            let mut new_lines = vec![];
-            for line in lines {
-                for line in line {
-                    new_lines.push(line);
-                }
-            }
-
-            let body_paragraph = Paragraph::new(new_lines).wrap(Wrap { trim: false });
-            f.render_widget(body_paragraph, text_rect);
-        } else {
-            let left_padding_percentage = {
-                let (width, height) = self.image.lock().unwrap().as_ref().unwrap().dimensions;
-                if width > height {
-                    40
-                } else {
-                    45
-                }
-            };
-            let [_, image_rect, _] = Layout::horizontal([
-                Constraint::Percentage(left_padding_percentage),
-                Constraint::Percentage(45),
-                Constraint::Percentage(10),
-            ])
-            .areas(inner_rect);
-            if let Some(image) = &mut *self.image.lock().unwrap() {
-                let image_state = StatefulImage::new(None);
-                f.render_stateful_widget(image_state, image_rect, &mut image.image);
-            }
-        }
-
-        self.is_focused = false;
     }
 }
