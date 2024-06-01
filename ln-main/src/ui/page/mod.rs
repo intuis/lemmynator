e2d450c8c5debbda25f95cc7f750fbe7677153db
Remigiusz Micielski <rmicielski@purelymail.com>
2024-06-01 13:56:10 +0200

    make page rendering code actually sane

--- ln-main/src/ui/page/mod.rs
+++ ln-main/src/ui/page/mod.rs
@@ -19,88 +19,28 @@ use crate::{action::Action, app::Ctx};
 
 pub struct Page {
     listing_type: ListingType,
-    page_data: Arc<Mutex<Option<PageData>>>,
-    posts_offset: usize,
-    currently_focused: u8,
-    currently_displaying: u8,
+    page_data: Arc<Mutex<PageData>>,
     can_fetch_new_pages: Arc<AtomicBool>,
     ctx: Arc<Ctx>,
 }
 
 struct PageData {
-    next_page: PaginationCursor,
     posts: Vec<LemmynatorPost>,
+    next_page: Option<PaginationCursor>,
+    posts_offset: usize,
+    currently_focused: u8,
+    currently_displaying: u8,
 }
 
 impl PageData {
-    fn new(posts: Vec<LemmynatorPost>, next_page: PaginationCursor) -> Self {
-        PageData { posts, next_page }
-    }
-}
-
-impl Page {
-    pub async fn new(listing_type: ListingType, ctx: Arc<Ctx>) -> Result<Self> {
-        Ok(Self {
-            listing_type,
-            page_data: Arc::new(Mutex::new(None)),
+    fn new() -> Self {
+        PageData {
+            posts: vec![],
+            next_page: None,
             posts_offset: 0,
             currently_focused: 0,
             currently_displaying: 0,
-            can_fetch_new_pages: Arc::new(AtomicBool::new(true)),
-            ctx: Arc::clone(&ctx),
-        })
-    }
-
-    async fn fetch_next_page(
-        page_data: Arc<Mutex<Option<PageData>>>,
-        atomic_lock: Arc<AtomicBool>,
-        ctx: Arc<Ctx>,
-        listing_type: ListingType,
-    ) {
-        let cursor = {
-            let page_data_lock = &mut *page_data.lock().unwrap();
-
-            if let Some(page_data) = page_data_lock {
-                Some(page_data.next_page.clone())
-            } else {
-                None
-            }
-        };
-
-        let posts_req = GetPosts {
-            type_: Some(listing_type),
-            sort: Some(lemmy_api_common::lemmy_db_schema::SortType::Hot),
-            page_cursor: cursor,
-            limit: Some(20),
-            ..Default::default()
-        };
-
-        let req = ctx
-            .client
-            .get(format!(
-                "https://{}/api/v3/post/list",
-                ctx.config.connection.instance
-            ))
-            .query(&posts_req);
-
-        let page: GetPostsResponse = req.send().await.unwrap().json().await.unwrap();
-
-        let mut new_posts = page
-            .posts
-            .into_iter()
-            .map(|post| LemmynatorPost::from_lemmy_post(post, Arc::clone(&ctx)))
-            .collect();
-
-        let page_data_lock = &mut *page_data.lock().unwrap();
-        if let Some(page_data) = page_data_lock {
-            page_data.posts.append(&mut new_posts);
-            page_data.next_page = page.next_page.unwrap();
-        } else {
-            *page_data_lock = Some(PageData::new(new_posts, page.next_page.unwrap()));
         }
-
-        atomic_lock.store(true, Ordering::SeqCst);
-        ctx.action_tx.send(Action::Render).unwrap();
     }
 
     fn scroll_up(&mut self) {
@@ -124,59 +64,10 @@ impl Page {
         self.currently_displaying = (rect.height / 8) as u8;
     }
 
-    fn render_loading_screen(&mut self, f: &mut Frame, rect: Rect) {
-        let loading_rect = centered_rect(rect, 50, 1);
-        let loading_paragraph = Paragraph::new("").alignment(Alignment::Center);
-        f.render_widget(loading_paragraph, loading_rect);
-    }
+    fn rects_for_posts(&mut self, mut rect_pool: Rect) -> Vec<Rect> {
+        let offseted_posts = &mut self.posts[self.posts_offset..];
+        let posts = &mut offseted_posts[..self.currently_displaying as usize];
 
-    fn render_posts(&mut self, f: &mut Frame, rect: Rect) {
-        self.update_count_of_currently_displaying(rect);
-
-        let page_data = &mut self.page_data.lock().unwrap();
-        let page_data = page_data.as_mut().unwrap();
-
-        let main_rect = rect;
-
-        let current_page = self.posts_offset / self.currently_displaying as usize;
-        if current_page > 3 {
-            page_data
-                .posts
-                .drain(0..2 * self.currently_displaying as usize);
-            self.posts_offset -= self.currently_displaying as usize * 2;
-        }
-
-        if page_data.posts.len() < self.posts_offset + self.currently_displaying as usize {
-            // Can't render a full page. Fetch new pages and return.
-            Self::try_fetch_new_pages(self);
-            return;
-        }
-
-        let offseted_posts = &mut page_data.posts[self.posts_offset..];
-
-        if offseted_posts
-            .get(2 * self.currently_displaying as usize)
-            .is_none()
-        {
-            // We are getting near the end of available pages, fetch new pages then
-            Self::try_fetch_new_pages(self);
-        }
-
-        let posts_to_display = &mut offseted_posts[..self.currently_displaying as usize];
-
-        let mut rects = self.rects_for_posts(&posts_to_display, rect);
-        let size_occupied = rects
-            .iter()
-            .map(|rect| rect.height)
-            .fold(0, |acc, height| acc + height);
-
-        let space_for_padding_available =
-            main_rect.height - size_occupied > self.currently_displaying as u16;
-
-        self.render_posts_in_layout(f, posts_to_display, &mut rects, space_for_padding_available);
-    }
-
-    fn rects_for_posts(&self, posts: &[LemmynatorPost], mut rect_pool: Rect) -> Vec<Rect> {
         let mut rects = vec![];
         for post in posts {
             let vertical_length = {
@@ -206,12 +97,14 @@ impl Page {
     }
 
     fn render_posts_in_layout(
-        &self,
+        &mut self,
         f: &mut Frame,
-        posts: &mut [LemmynatorPost],
         rects: &mut [Rect],
         space_for_padding_available: bool,
     ) {
+        let offseted_posts = &mut self.posts[self.posts_offset..];
+        let posts = &mut offseted_posts[..self.currently_displaying as usize];
+
         let mut current_offset = 0;
         let mut index = 0;
         for (post, rect) in posts.iter_mut().zip(rects.into_iter()) {
@@ -243,7 +136,112 @@ impl Page {
     }
 }
 
+impl Page {
+    pub async fn new(listing_type: ListingType, ctx: Arc<Ctx>) -> Result<Self> {
+        Ok(Self {
+            listing_type,
+            page_data: Arc::new(Mutex::new(PageData::new())),
+            can_fetch_new_pages: Arc::new(AtomicBool::new(true)),
+            ctx: Arc::clone(&ctx),
+        })
+    }
+
+    async fn fetch_next_page(
+        page_data: Arc<Mutex<PageData>>,
+        atomic_lock: Arc<AtomicBool>,
+        ctx: Arc<Ctx>,
+        listing_type: ListingType,
+    ) {
+        let cursor = {
+            let page_data_lock = &mut *page_data.lock().unwrap();
+            page_data_lock.next_page.clone()
+        };
+
+        let posts_req = GetPosts {
+            type_: Some(listing_type),
+            sort: Some(lemmy_api_common::lemmy_db_schema::SortType::Hot),
+            page_cursor: cursor,
+            limit: Some(20),
+            ..Default::default()
+        };
+
+        let req = ctx
+            .client
+            .get(format!(
+                "https://{}/api/v3/post/list",
+                ctx.config.connection.instance
+            ))
+            .query(&posts_req);
+
+        let page: GetPostsResponse = req.send().await.unwrap().json().await.unwrap();
+
+        let mut new_posts = page
+            .posts
+            .into_iter()
+            .map(|post| LemmynatorPost::from_lemmy_post(post, Arc::clone(&ctx)))
+            .collect();
+
+        let page_data_lock = &mut *page_data.lock().unwrap();
+        page_data_lock.posts.append(&mut new_posts);
+        page_data_lock.next_page = Some(page.next_page.unwrap());
+
+        atomic_lock.store(true, Ordering::SeqCst);
+        ctx.action_tx.send(Action::Render).unwrap();
+    }
+
+    fn render_loading_screen(&mut self, f: &mut Frame, rect: Rect) {
+        let loading_rect = centered_rect(rect, 50, 1);
+        let loading_paragraph = Paragraph::new("").alignment(Alignment::Center);
+        f.render_widget(loading_paragraph, loading_rect);
+    }
+}
+
 impl Component for Page {
+    fn handle_actions(&mut self, action: Action) -> Option<Action> {
+        self.page_data.lock().unwrap().handle_actions(action)
+    }
+
+    fn render(&mut self, f: &mut Frame, rect: Rect) {
+        let [posts_rect, bottom_bar_rect] =
+            Layout::vertical([Constraint::Percentage(100), Constraint::Length(1)]).areas(rect);
+
+        let mut pages_available;
+
+        {
+            if !self.page_data.lock().unwrap().posts.is_empty() {
+                pages_available = true;
+
+                let page_data_lock = self.page_data.lock().unwrap();
+                if page_data_lock.posts.len()
+                    < page_data_lock.posts_offset + page_data_lock.currently_displaying as usize
+                {
+                    self.try_fetch_new_pages();
+                    pages_available = false;
+                } else if page_data_lock.posts.len()
+                    < page_data_lock.posts_offset + page_data_lock.currently_displaying as usize * 2
+                {
+                    self.try_fetch_new_pages();
+                }
+            } else {
+                pages_available = false;
+            }
+        }
+
+        if pages_available {
+            self.page_data.lock().unwrap().render(f, posts_rect)
+        } else {
+            self.try_fetch_new_pages();
+            self.render_loading_screen(f, posts_rect);
+        }
+
+        self.page_data
+            .lock()
+            .unwrap()
+            .render_bottom_bar(f, bottom_bar_rect);
+    }
+}
+
+impl Component for PageData {
     fn handle_actions(&mut self, action: Action) -> Option<Action> {
         match action {
             Action::Up => {
@@ -259,28 +257,26 @@ impl Component for Page {
     }
 
     fn render(&mut self, f: &mut Frame, rect: Rect) {
-        let [posts_rect, bottom_bar_rect] =
-            Layout::vertical([Constraint::Percentage(100), Constraint::Length(1)]).areas(rect);
+        self.update_count_of_currently_displaying(rect);
 
-        let mut pages_available;
-        if let Some(page_data) = &*self.page_data.lock().unwrap() {
-            pages_available = true;
-            if page_data.posts.len() < self.posts_offset + self.currently_displaying as usize {
-                Self::try_fetch_new_pages(self);
-                pages_available = false;
-            }
-        } else {
-            pages_available = false;
+        let main_rect = rect;
+
+        let current_page = self.posts_offset / self.currently_displaying as usize;
+        if current_page > 3 {
+            self.posts.drain(0..2 * self.currently_displaying as usize);
+            self.posts_offset -= self.currently_displaying as usize * 2;
         }
 
-        if pages_available {
-            self.render_posts(f, posts_rect)
-        } else {
-            self.try_fetch_new_pages();
-            self.render_loading_screen(f, posts_rect);
-        }
+        let mut rects = self.rects_for_posts(rect);
+        let size_occupied = rects
+            .iter()
+            .map(|rect| rect.height)
+            .fold(0, |acc, height| acc + height);
 
-        self.render_bottom_bar(f, bottom_bar_rect)
+        let space_for_padding_available =
+            main_rect.height - size_occupied > self.currently_displaying as u16;
+
+        self.render_posts_in_layout(f, &mut rects, space_for_padding_available);
     }
 }
 
