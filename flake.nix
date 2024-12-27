cc7e1d11df39b5dca7fc2fa7d760a172219cb03b
micielski <73398428+micielski@users.noreply.github.com>
2024-12-27 10:51:47 +0000

    nix: init (#17)

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Lemmynator";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url ="github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            rust-bin.stable.latest.default
+            rust-analyzer
+          ];
+        };
+      }
+    );
+}
+
